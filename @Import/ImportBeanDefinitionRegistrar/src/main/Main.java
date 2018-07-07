a61344e79a0e448a21cfcd6c7d0b2b58df9769be
y1笑而过 <15335747148@163.com>
2018-07-07 11:41:06 +0800

    ImportBeanDefinitionRegistrar
    ImportBeanDefinitionRegistrar

---
+++ @Import/ImportBeanDefinitionRegistrar/src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String name: beanDefinitionNames) {
+            System.out.println(name);
+        }
+        //org.springframework.context.annotation.internalConfigurationAnnotationProcessor
+        //org.springframework.context.annotation.internalAutowiredAnnotationProcessor
+        //org.springframework.context.annotation.internalRequiredAnnotationProcessor
+        //org.springframework.context.event.internalEventListenerProcessor
+        //org.springframework.context.event.internalEventListenerFactory
+        //appConfig
+        //main.entities.Red
+        //main.entities.Blue
+        //main.entities.Green
+        //main.entities.Black
+        //purple
+    }
+}
