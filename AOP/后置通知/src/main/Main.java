c953a944d25075289dfa6606e2cfb088c098679d
y1笑而过 <15335747148@163.com>
2018-07-08 16:45:43 +0800

    后置通知
    后置通知

---
+++ AOP/后置通知/src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    /**
+     * 1.书写ArithmeticCalculator接口
+     * 2.书写ArithmeticCalculatorImpl，并加上@Component注解或在配置类中注册Bean
+     * 3.下载AspectJ的jar包并加入工程依赖
+     * 4.书写LogAspect并加上@Aspect注解，并加上@Component注解或中配置类中注册Bean
+     * 5.书写配置类加上@EnableAspectJAutoProxy注解
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        ArithmeticCalculator arithmeticCalculator = (ArithmeticCalculator) ctx.getBean("arithmeticCalculator");
+        double result = arithmeticCalculator.add(1, 2);
+        System.out.println(result);
+
+        //LogAspect beforeLog: add --- [1.0, 2.0]
+        //LogAspect 前置通知
+        //3.0
+    }
+}
